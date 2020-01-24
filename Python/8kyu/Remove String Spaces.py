f1aefe813b4c7c25ad53db1cc93bf4532a3f2247
Jordan Danaher <jordanaher@gmail.com>
2020-01-24 14:24:43 -0500

    Challenge started but incomplete

---
+++ Python/8kyu/Remove String Spaces.py
@@ -0,0 +1,8 @@
+# =============================================================================
+# Remove String Spaces
+# 
+# Simple, remove the spaces from the string, then return the resultant string.
+# =============================================================================
+
+def no_space(x):
+    x.index(" ")
