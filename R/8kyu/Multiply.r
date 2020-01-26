02a756d1f6c686466b26e6db70050927dde5799d
Jordan Danaher <jordanaher@gmail.com>
2020-01-26 14:24:24 -0500

    Challenge complete

---
+++ R/8kyu/Multiply.r
@@ -0,0 +1,13 @@
+# Multiply
+# 
+# The code does not execute properly. Try to figure out why.
+#
+# mul <- function(a, b) {
+#   a * c # try to figure out why it doesn't work!
+# }
+
+mul <- function(a, b) {
+  a * b # try to figure out why it doesn't work!
+}
+
+
