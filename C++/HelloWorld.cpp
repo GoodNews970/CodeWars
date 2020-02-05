b2a8bf6a050688c89572713060d77ad49f04986c
Jordan Danaher <jordanaher@gmail.com>
2020-02-05 15:22:53 -0500

    Testing for java, c and C++

--- C++/HelloWorld.cpp
+++ C++/HelloWorld.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main() 
 {
     cout << "Hello, World!";
+    cin.get(); // pause for input
     return 0;
 }
--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -2,5 +2,6 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
+   getchar(); // pause for input
    return 0;
 }
