449eb73c386fa35473e20cbf3d2908cd3603a23d
Jordan Danaher <jordanaher@gmail.com>
2020-02-06 12:51:29 -0500

    Challenge complete

--- C/8kyu/Count_of_positives__sum_of_negatives.c
+++ C/8kyu/Count_of_positives__sum_of_negatives.c
@@ -10,34 +10,18 @@ Example
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65]. */
 
 #include <stddef.h>
-#include <stdio.h>
 
-int count_positives_sum_negatives(int *values, size_t count, int *positivesCount, int *negativesSum) 
+void count_positives_sum_negatives(int *values, size_t count, int *positivesCount, int *negativesSum)
 {
-    int pc=0, ns=0;
     for (size_t i = 0; i < count; i++)
     {
-       if (values[i] < 0)
-       {
-           ns += values[i];
-       } 
-       else 
-       {
-           pc += 1;
-       }
+        if (values[i] < 0)
+        {
+            *negativesSum += values[i];
+        }
+        if (values[i] > 0)
+        {
+            *positivesCount += 1;
+        }
     }
-    
-    positivesCount = &pc;
-    negativesSum = &ns;
-    int result[] = {*positivesCount, *negativesSum};
-    return result;
-}  
-
-int main(void)
-{
-    int array[] = {1,2,3,4,-5,-5};
-    size_t count = 6;
-    int *positivesCount;
-    int *negativeSum;
-    count_positives_sum_negatives(array, count, positivesCount, negativeSum);
 }
