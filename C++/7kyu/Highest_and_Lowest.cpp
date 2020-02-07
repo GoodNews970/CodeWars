a1b46c96369b63a2b62087e3197bd1c85c12a9d3
Jordan Danaher <jordanaher@gmail.com>
2020-02-07 15:54:09 -0500

    Final solution refactored (conditional operator)

--- C++/7kyu/Highest_and_Lowest.cpp
+++ C++/7kyu/Highest_and_Lowest.cpp
@@ -13,7 +13,6 @@ All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first. */
 
-#include <iostream>
 #include <string>
 #include <cstring>
 
@@ -21,34 +20,15 @@ using std::string;
 
 string highAndLow(string numbers)
 {
-    std::cout << numbers << "\n";
-    char copy[numbers.length()];
+    char copy[numbers.length() * 1000];
     size_t length = numbers.copy(copy, numbers.length(), 0);
     char *token = strtok(copy, " ");
     int max = -10000, min = 10000;
     while (token != NULL)
     {
-        std::cout << "Current token: " << token << "\n";
-        if (atoi(token) > max)
-        {
-            std::cout << "Previous max: " << max << "\n";
-            max = atoi(token);
-            std::cout << "New max: " << max << "\n";
-        }
-        if (atoi(token) < min)
-        {
-            std::cout << "Previous min: " << min << "\n";
-            min = atoi(token);
-            std::cout << "New min: " << min << "\n";
-        }
+        max = (atoi(token) > max) ? max = atoi(token) : max = max;
+        min = (atoi(token) < min) ? min = atoi(token) : min = min;
         token = strtok(NULL, " ");
     }
-    std::string result = std::to_string(max) + " " + std::to_string(min);
-    return (result);
-}
-
-int main()
-{
-    std::string result = highAndLow("1 -1 0");
-    std::cout << result;
+    return (std::to_string(max) + " " + std::to_string(min));
 }
