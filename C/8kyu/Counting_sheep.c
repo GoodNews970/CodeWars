ba2731bb97d1f686112e308be605c517517b1f55
Jordan Danaher <jordanaher@gmail.com>
2020-02-07 11:27:50 -0500

    Challenge complete

---
+++ C/8kyu/Counting_sheep.c
@@ -0,0 +1,30 @@
+/* Counting sheep
+
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+{ true,  true,  true,  false,
+  true,  true,  true,  true,
+  true,  false, true,  false,
+  true,  false, false, true,
+  true,  true,  true,  true,
+  false, false, true,  true }
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined */
+
+#include <stdbool.h>
+#include <stddef.h>
+
+size_t count_sheep(const bool *sheep, size_t count)
+{
+    size_t tc = 0;
+    for (size_t i = 0; i < count; i++)
+    {
+        if (sheep[i]){
+            tc += 1;
+        }
+    }
+    return tc;
+}
