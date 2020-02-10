b08810909191fe13fdf774bf4c6d696284f1d5e3
Jordan Danaher <jordanaher@gmail.com>
2020-02-10 11:37:30 -0500

    Challenge complete

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,10 +1,9 @@
 // Main class to test kata classes.
 
-import _7kyu.*;
+// import _6kyu.*;
 
 public class Main {
     public static void main(final String args[]) {
-        System.out.println(Dont_give_me_five.dontGiveMeFive(1, 25));
     }
     
 }
