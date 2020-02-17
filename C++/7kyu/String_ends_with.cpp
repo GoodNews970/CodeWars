e54b61792a87a76c2a760b2109f3fa87494cf4d1
Jordan Danaher <jordanaher@gmail.com>
2020-02-17 11:48:56 -0500

    Challenge complete

---
+++ C++/7kyu/String_ends_with.cpp
@@ -0,0 +1,16 @@
+/* String ends with?
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument
+(also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false */
+
+#include <string>
+
+bool solution(std::string const &str, std::string const &ending)
+{
+    return (str.length() >= ending.length()) ? str.substr(str.length() - ending.length(), ending.length()) == ending : false;
+}
