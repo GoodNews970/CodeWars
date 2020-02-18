5bd2f9e9dca4a650b54ae3e7ed3ea0598895d125
Jordan Danaher <jordanaher@gmail.com>
2020-02-18 16:27:11 -0500

    Challenge complete

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,9 +1,11 @@
 // Main class to test kata classes.
 
-// import _6kyu.*;
+// import _7kyu.Moves_in_squared_strings_I;
 
 public class Main {
+
     public static void main(final String args[]) {
+        
     }
     
 }
