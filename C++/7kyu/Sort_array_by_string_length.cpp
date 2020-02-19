3a4dfa84ce9cb8e073be2ac8109ae7c32978f637
Jordan Danaher <jordanaher@gmail.com>
2020-02-19 14:23:45 -0500

    Challenge complete

---
+++ C++/7kyu/Sort_array_by_string_length.cpp
@@ -0,0 +1,42 @@
+/* Sort array by string length
+
+Write a function that takes an array of strings as an argument and returns a
+sorted array containing the same strings, ordered from shortest to longest.
+
+For example, if this array were passed as an argument:
+
+["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+Your function would return the following array:
+
+["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+All of the strings in the array passed to your function will be different
+lengths, so you will not have to decide how to order multiple strings of the
+same length. */
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using std::sort;
+using std::string;
+using std::vector;
+
+class Kata
+{
+public:
+    vector<string> sortByLength(vector<string> array)
+    {
+        struct compare
+        {
+            bool operator()(const std::string &first, const std::string &second)
+            {
+                return first.size() < second.size();
+            }
+        };
+        compare c;
+        sort(array.begin(), array.end(), c);
+        return array;
+    }
+};
