a9782e721cf62e095685266d37a0747e032d54c1
Jordan Danaher <jordanaher@gmail.com>
2020-02-20 12:16:26 -0500

    Challenge complete

---
+++ C++/7kyu/Simple_Fun__176__Reverse_Letter.cpp
@@ -0,0 +1,34 @@
+/* Simple Fun #176: Reverse Letter
+
+Task
+Given a string str, reverse it omitting all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string */
+
+#include <string>
+
+using std::string;
+
+std::string reverse_letter(const std::string &str)
+{
+    string ret = "";
+    for (int i = str.length() - 1; i >= 0; --i)
+    {
+        if ((str[i] >= 'A' && str[i] <= 'Z') ||
+            (str[i] >= 'a' && str[i] <= 'z'))
+        {
+            ret += str[i];
+        }
+    }
+    return ret;
+}
