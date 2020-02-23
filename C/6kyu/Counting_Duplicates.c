bf09a0b58f7897f3956c6aa86b2d5960f8b2321b
Jordan Danaher <jordanaher@gmail.com>
2020-02-23 12:50:04 -0500

    Challenge complete

---
+++ C/6kyu/Counting_Duplicates.c
@@ -0,0 +1,43 @@
+/* Count the number of Duplicates
+
+Write a function that will return the count of distinct case-insensitive
+alphabetic characters and numeric digits that occur more than once in the input
+string. The input string can be assumed to contain only alphabets
+(both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice */
+
+#include <stddef.h>
+#include <string.h>
+#include <ctype.h>
+
+size_t duplicate_count(const char *text)
+{
+    int ret = 0;
+    char dupes[1000] = "", c[2] = "";
+    for (size_t i = 0; i < strlen(text); i++)
+    {
+        if (strchr(dupes, tolower(text[i])))
+        {
+            continue;
+        }
+        for (size_t j = i + 1; j < strlen(text); j++)
+        {
+            if (tolower(text[i]) == tolower(text[j]))
+            {
+                ret++;
+                c[0] = tolower(text[i]);
+                strcat(dupes, c);
+                break;
+            }
+        }
+    }
+    return ret;
+}
