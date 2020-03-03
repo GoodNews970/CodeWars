b733027f4d7d71f156c5c8d3dbfd9991a54ac8c2
Jordan Danaher <jordanaher@gmail.com>
2020-03-03 08:26:28 -0500

    Challenge complete

---
+++ C++/6kyu/Count_the_smiley_faces.cpp
@@ -0,0 +1,49 @@
+/* Count the smiley faces!
+
+Description:
+Given an array (arr) as an argument complete the function countSmileys that
+should return the total number of smiling faces.
+Rules for a smiling face:
+-Each smiley face must contain a valid pair of eyes.
+Eyes can be marked as : or ;
+-A smiley face can have a nose but it does not have to. Valid characters for a
+nose are - or ~
+-Every smiling face must have a smiling mouth that should be marked with
+either ) or D.
+No additional characters are allowed except for those mentioned.
+Valid smiley face examples:
+:) :D ;-D :~)
+Invalid smiley faces:
+;( :> :} :]
+
+Example cases:
+
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+
+Note: In case of an empty array return 0. You will not be tested with invalid
+input (input will always be an array). Order of the face (eyes, nose, mouth)
+elements will always be the same
+Happy coding! */
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using std::count_if;
+using std::find;
+using std::string;
+using std::vector;
+
+bool isValid(string face)
+{
+    vector<string> valid = {":D", ":-D", ":~D", ";D", ";-D", ";~D",
+                            ":)", ":-)", ":~)", ";)", ";-)", ";~)"};
+    return find(valid.begin(), valid.end(), face) != valid.end();
+}
+
+int countSmileys(vector<string> arr)
+{
+    return count_if(arr.begin(), arr.end(), isValid);
+}
